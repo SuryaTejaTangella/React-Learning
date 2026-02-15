d75e39def07c94da40786d865b7de0406e4a78b1
Surya Teja Tangella <suryatejatangella234@gmail.com>
2026-02-15 12:48:16 +0530

    Added React Project

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Welcome } from "./Welcome";
+import { Button } from "./Button";
+import { Hello, HelloWithoutJSX } from "./Hello";
+import { UserProfile } from "./UserProfile";
+
+function App() {
+  return (
+    <div>
+      <UserProfile />
+      <Hello />
+      <HelloWithoutJSX />
+      <Welcome />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
